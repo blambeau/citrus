aaf57674af4a529245af9c9b5336152af4e5e1dc
Bernard Lambeau <blambeau@gmail.com>
2010-11-23 16:58:55 +0100

    Optimize sexp grammar with regular expressions.

--- benchmark/sexp/sexp.rb
+++ benchmark/sexp/sexp.rb
@@ -4,7 +4,7 @@ require 'citrus'
 module Sexp
 
   # Version of this Sexp grammar
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 
   # Load and evaluate the grammars contained in sexp.citrus into 
   # the global namespace.
@@ -98,6 +98,12 @@ module Sexp
     def consume(text, rule = :expression)
       Sexp::parse(text, :root => rule, :consume => true)
     end
+    
+    def test_expression_on_random
+      assert_nothing_raised{
+        consume(Sexp::generate(64), :expression)
+      }
+    end
   
     def test_expression
       [ 
@@ -124,7 +130,7 @@ module Sexp
         assert_equal p.to_i, consume(p, :INTEGER).to_i
       }
     end
-  
+      
     def test_double_quoted_string
       [ %q{""}, 
         %q{"hello"},
@@ -133,7 +139,7 @@ module Sexp
         assert_nothing_raised{ consume(p, :DOUBLE_QUOTED_STRING) }
       }
     end
-  
+      
     def test_single_quoted_string
       [ %q{''}, 
         %q{'hello'},
@@ -142,13 +148,13 @@ module Sexp
         assert_nothing_raised{ consume(p, :SINGLE_QUOTED_STRING) }
       }
     end
-  
+      
     def test_variable
       [ "a", "hello", "hello_world" ].each{|p| 
         assert_nothing_raised{ consume(p, :VARIABLE) }
       }
     end
-  
+      
     def test_module
       [ "::A", "A", "Hello", "HelloWorld", "Hello::World", "::Hello::World" ].each{|p| 
         assert_nothing_raised{ consume(p, :MODULE) }
