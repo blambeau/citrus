e1396979e10f53eed286ddb6e5c83d22fa78a55b
Bernard Lambeau <blambeau@gmail.com>
2010-11-23 12:45:42 +0100

    Moves seqpar benchmark in its own folder.

--- benchmark/seqpar/seqpar.rb
+++ benchmark/seqpar/seqpar.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH << File.expand_path('../../lib', __FILE__)
+$LOAD_PATH << File.expand_path('../../../lib', __FILE__)
 
 # Benchmarking written by Bernard Lambeau and Jason Garber of the Treetop
 # project.
